eaacaeda8914e2ba01c635bb672fa8ed9cf3dab6
admin-yxk <yexinke16@gmail.com>
2026-05-20 21:33:53 +0800

    测试接口

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @PostMapping("/test")
+    public String test() {
+        return testService.test();
+    }
+}
